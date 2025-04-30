bfb1dd03cb2ef1ee9560a053264884c50bf454f7
Flávio Dutra <flaviomdutra@outlook.com>
2025-04-29 21:05:36 -0300

    feat(server): enhance error handling and graceful shutdown in server initialization

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,26 @@ import { Server } from '@overnightjs/core';
 import * as database from '@src/database';
 import bodyParser from 'body-parser';
 import { Application } from 'express';
+import * as http from 'http';
 import { BeachesController } from './controllers/beaches';
 import { ForecastController } from './controllers/forecast';
 import { UsersController } from './controllers/users';
 import logger from './logger';
 
 export class SetupServer extends Server {
+  private server?: http.Server;
+  /*
+   * same as this.port = port, declaring as private here will
+   * add the port variable to the SetupServer instance
+   */
   constructor(private port = 3000) {
     super();
   }
 
+  /*
+   * We use a different method to init instead of using the constructor
+   * this way we allow the server to be used in tests and normal initialization
+   */
   public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
@@ -22,6 +32,7 @@ export class SetupServer extends Server {
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
+    this.setupControllers();
   }
 
   private setupControllers(): void {
@@ -35,21 +46,31 @@ export class SetupServer extends Server {
     ]);
   }
 
+  public getApp(): Application {
+    return this.app;
+  }
+
   private async databaseSetup(): Promise<void> {
     await database.connect();
   }
 
   public async close(): Promise<void> {
     await database.close();
+    if (this.server) {
+      await new Promise((resolve, reject) => {
+        this.server?.close((err) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(true);
+        });
+      });
+    }
   }
 
   public start(): void {
-    this.app.listen(this.port, () => {
-      logger.info('Server listening of port:', this.port);
+    this.server = this.app.listen(this.port, () => {
+      logger.info('Server listening on port: ' + this.port);
     });
-  }
-
-  public getApp(): Application {
-    return this.app;
   }
 }
