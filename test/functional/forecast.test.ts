751babc67f76f01f830647251990b8c0490a3936
Flávio Dutra <flaviomdutra@outlook.com>
2025-05-03 11:01:12 -0300

    refactor(beach): rename BeachPosition to GeoPosition and update references across models and tests

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,4 +1,4 @@
-import { Beach, BeachPosition } from '@src/models/beach';
+import { Beach, GeoPosition } from '@src/models/beach';
 import { User } from '@src/models/user';
 import { AuthService } from '@src/services/auth';
 import nock from 'nock';
@@ -20,7 +20,7 @@ describe('Beach forecast functional tests', () => {
       lat: -33.792726,
       lng: 151.289824,
       name: 'Manly',
-      position: BeachPosition.E,
+      position: GeoPosition.E,
       user: user.id,
     };
     await new Beach(defaultBeach).save();
